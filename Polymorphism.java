cefbe5e210f98569d7ae7aa8de55ed24673e1fcf
aelraffa <143625834+aelraffa@users.noreply.github.com>
2024-09-10 08:01:55 +0700

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,62 +1,29 @@
-class Mhs {
-    private String NamaMhs;
-    private int UmurMhs;
-
-    public String getNamaMhs(){
-        return NamaMhs;
-    }
-
-    public void setNamaMhs(String NamaMhs){
-        this.NamaMhs = NamaMhs;
-    }
-
-    public int getUmurMhs(){
-        return UmurMhs;
-    }
-
-    public void setUmurMhs(int UmurMhs){
-        this.UmurMhs = UmurMhs;
-    }
-
-    public void tampilkanInfo() {
-        System.out.println("Nama Mahasiswa: " + NamaMhs);
-        System.out.println("Umur: " + UmurMhs);
+class Hewan {
+    void suara() {
+        System.out.println("Hewan bersuara");
     }
 }
 
-class MhsJurusan extends Mhs {
-    private String jurusan;
-
-    public String getJurusan() {
-        return jurusan;
-    }
-
-    public void setJurusan(String jurusan) {
-        this.jurusan = jurusan;
-    }
-
+class Anjing extends Hewan {
     @Override
-    public void tampilkanInfo() {
-        super.tampilkanInfo();  
-        System.out.println("Jurusan: " + jurusan);
+    void suara() {
+        System.out.println("Anjing menggonggong");
     }
 }
 
-public class Polymorphism {
+class Kucing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Kucing mengeong");
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-       
-        MhsJurusan mahasiswa1 = new MhsJurusan();
-        mahasiswa1.setNamaMhs("Toni");
-        mahasiswa1.setUmurMhs(19);
-        mahasiswa1.setJurusan("Teknik Informatika");
-
-        MhsJurusan mahasiswa2 = new MhsJurusan();
-        mahasiswa2.setNamaMhs("Budi");
-        mahasiswa2.setUmurMhs(21);
-        mahasiswa2.setJurusan("Teknik Elektro");
-
-        mahasiswa1.tampilkanInfo();
-        System.out.println("\n");
-        mahasiswa2.tampilkanInfo();
+        Hewan hewan1 = new Anjing();
+        Hewan hewan2 = new Kucing();
+        
+        hewan1.suara();  // Output: Anjing menggonggong
+        hewan2.suara();  // Output: Kucing mengeong
     }
 }
